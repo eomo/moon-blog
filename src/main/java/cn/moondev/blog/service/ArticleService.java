b53998c7918e91dfc6be1bba5bf1cb0e5f9b050d
skye <skye@lwork.com>
2018-09-05 14:55:04 +0800

    ADDED:文章发布时间优化

--- src/main/java/cn/moondev/blog/service/ArticleService.java
+++ src/main/java/cn/moondev/blog/service/ArticleService.java
@@ -115,7 +115,7 @@ public class ArticleService extends BaseService {
         tmp.aformat = article.aformat;
         tmp.mformat = article.mformat;
         tmp.updatedTime = LocalDateTime.now();
-        tmp.publishTime = LocalDateTime.now();
+        tmp.publishTime = Objects.isNull(tmp.publishTime) ? LocalDateTime.now() : tmp.publishTime;
         tmp.status = 1;
         mapper.upsert(tmp);
         // 如果重新发布文章，清理缓存中的内容
@@ -149,7 +149,7 @@ public class ArticleService extends BaseService {
         tmp.aformat = article.aformat;
         tmp.mformat = article.mformat;
         tmp.updatedTime = LocalDateTime.now();
-        tmp.publishTime = LocalDateTime.now();
+        tmp.publishTime = Objects.isNull(tmp.publishTime) ? LocalDateTime.now() : tmp.publishTime;
         tmp.status = 1;
         mapper.upsert(tmp);
         // 如果重新发布文章，清理缓存中的内容
